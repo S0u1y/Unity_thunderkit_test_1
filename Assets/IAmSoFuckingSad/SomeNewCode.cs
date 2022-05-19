ccda01967332ccce4115aee5a06de472d211df0e
S0u1y <105646357+S0u1y@users.noreply.github.com>
2022-05-19 15:27:57 +0200

    what the hecc am I doing ?
    added uhh.. bepinex reload and run,
    a serializable content pack,
    Hook and API packages...
    trying out including some "basic mod" into the run but I have no clue how :)

---
+++ Assets/IAmSoFuckingSad/SomeNewCode.cs
@@ -0,0 +1,77 @@
+﻿//using RoR2.ContentManagement;
+//using RoR2;
+//using System.Collections;
+//using System.Reflection;
+//using UnityEngine;
+//using Path = System.IO.Path;
+
+//public class SomeNewCode
+//{
+//    public static class Assets
+//    {
+//        public static AssetBundle mainAssetBundle = null;
+//        //the filename of your assetbundle
+//        internal static string assetBundleName = "IAmSoFuckingSad";
+
+//        internal static string assemblyDir
+//        {
+//            get
+//            {
+//                return Path.GetDirectoryName(Main.Info.Location);
+//            }
+//        }
+
+//        public static void PopulateAssets()
+//        {
+//            mainAssetBundle = AssetBundle.LoadFromFile(Path.Combine(assemblyDir, assetBundleName));
+//            ContentPackProvider.serializedContentPack = mainAssetBundle.LoadAsset<SerializableContentPack>(ContentPackProvider.contentPackName);
+//        }
+//    }
+
+//    public class ContentPackProvider : IContentPackProvider
+//    {
+//        public static SerializableContentPack serializedContentPack;
+//        public static ContentPack contentPack;
+//        //Should be the same names as your SerializableContentPack in the asset bundle
+//        public static string contentPackName = "MuchContent";
+
+//        public string identifier
+//        {
+//            get
+//            {
+//                //If I see this name while loading a mod I will make fun of you
+//                return "YuumiMod";
+//            }
+//        }
+
+//        internal static void Initialize()
+//        {
+//            contentPack = serializedContentPack.CreateContentPack();
+//            ContentManager.collectContentPackProviders += AddCustomContent;
+//        }
+
+//        private static void AddCustomContent(ContentManager.AddContentPackProviderDelegate addContentPackProvider)
+//        {
+//            addContentPackProvider(new ContentPackProvider());
+//        }
+
+//        public IEnumerator LoadStaticContentAsync(LoadStaticContentAsyncArgs args)
+//        {
+//            args.ReportProgress(1f);
+//            yield break;
+//        }
+
+//        public IEnumerator GenerateContentPackAsync(GetContentPackAsyncArgs args)
+//        {
+//            ContentPack.Copy(contentPack, args.output);
+//            args.ReportProgress(1f);
+//            yield break;
+//        }
+
+//        public IEnumerator FinalizeAsync(FinalizeAsyncArgs args)
+//        {
+//            args.ReportProgress(1f);
+//            yield break;
+//        }
+//    }
+//}
